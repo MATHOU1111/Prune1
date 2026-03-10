0fd729389b71b7d60242a30154c427486998ab75
mathou <mathisdumage0@gmail.com>
2026-03-10 08:55:41 +0100

    Debut d'un routing + prise en main du framework

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { FilmList } from './film-list/film-list';
+import { film-detail } from './film-detail/film-detail';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/films', pathMatch: 'full' },
   { path: 'films', component: FilmList }
+  { path: 'films/:id', component: fim-detail }
 ];
---
+++ frontend/src/app/film-detail/film-detail.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-detail',
+  imports: [],
+  templateUrl: './film-detail.html',
+  styleUrl: './film-detail.scss',
+})
+export class FilmDetail implements OnInit {
+
+
+
+  const id = params.get('id');
+
+  console.log(id);
+
+
+  }
